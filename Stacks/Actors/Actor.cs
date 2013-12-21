0de2985911801b50f8d1f0012e3d2933038ec669
Marcin Deptuła <marcin.deptula@live.com>
2013-12-21 22:54:07 +0100

    Added actor class

---
+++ Stacks/Actors/Actor.cs
@@ -0,0 +1,49 @@
+﻿using Stacks.Executors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stacks.Actors
+{
+    public class Actor
+    {
+        private readonly ActorContext context;
+        private readonly string name;
+
+        public Actor()
+            : this(null, 
+                   new ActionBlockExecutor(null, ActionContextExecutorSettings.Default))
+        { }
+
+        public Actor(string name)
+            : this(name, 
+                   new ActionBlockExecutor(name, ActionContextExecutorSettings.Default))
+        { }
+
+        public Actor(string name, IExecutor executor)
+        {
+            this.name = name;
+            this.context = new ActorContext(name, executor);
+        }
+
+        protected Task Completion { get { return context.Completion; } }
+
+        protected Task Stop()
+        {
+            return context.Stop();
+        }
+
+        public bool Named { get { return name != null; } }
+        public string Name { get { return name; } }
+
+        protected IActorContext Context { get { return context; } }
+        protected SynchronizationContext GetActorSynchronizationContext()
+        {
+            return context.Context;
+        }
+    }
+
+}
