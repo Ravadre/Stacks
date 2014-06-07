39d346ef96c495642e875184d9937def349266c7
Marcin Deptuła <marcin.deptula@live.com>
2014-06-08 00:59:23 +0200

    Fun with code gen

---
+++ Stacks.Tests/Client/ReactiveMessageClientTests.cs
@@ -0,0 +1,76 @@
+﻿using Moq;
+using MsgPack.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Stacks.Tcp;
+using System.Reactive.Subjects;
+
+namespace Stacks.Tests
+{
+    public class ReactiveMessageClientTests
+    {
+        public class Base
+        {
+            protected Mock<IRawByteClient> rawClient;
+            protected FramedClient framedClient;
+            protected Mock<IStacksSerializer> serializer;
+
+            protected Subject<ArraySegment<byte>> rawClientReceived;
+
+            public Base()
+            {
+                rawClientReceived = new Subject<ArraySegment<byte>>();
+                rawClient = new Mock<IRawByteClient>();
+                rawClient.Setup(r => r.Received).Returns(rawClientReceived);
+
+                framedClient = new FramedClient(rawClient.Object);
+                serializer = new Mock<IStacksSerializer>();
+            }
+
+            protected TestData CreateSampleTestData()
+            {
+                return new TestData { Bar = Math.PI, Zar = (decimal)Math.PI, Foo = 42, Sar = "Foo bar test" };
+            }
+        }
+
+        public class Receive : Base
+        {
+            [Fact]
+            public void Receiving_packet_should_be_deserialized_properly()
+            {
+                serializer.Setup(s => s.CreateDeserializer<TestData>()).Returns((MemoryStream ms) => new TestData());
+
+                var c = new ReactiveMessageClient<ITestMessageHandler>(framedClient, serializer.Object);
+
+            }
+        }
+
+        [StacksMessage(3)]
+        public class TestData
+        {
+            public int Foo { get; set; }
+            public double Bar { get; set; }
+            public decimal Zar { get; set; }
+            public string Sar { get; set; }
+        }
+
+        public interface ITestMessageHandler
+        {
+            IObservable<TestData> Packet1 { get; }
+        }
+
+        public class TestImpl : ITestMessageHandler
+        {
+            public IObservable<TestData> Packet1
+            {
+                get { return null; }
+            }
+        }
+
+    }
+}
--- Stacks/Client/Tcp/ReactiveMessageClient.cs
+++ Stacks/Client/Tcp/ReactiveMessageClient.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reactive;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,8 @@ namespace Stacks.Tcp
             : base(framedClient, new MessageIdCache(), packetSerializer)
         {
             this.framedClient.Received.Subscribe(PacketReceived);
+
+            Foo();
         }
 
         private unsafe void PacketReceived(ArraySegment<byte> buffer)
@@ -30,6 +34,45 @@ namespace Stacks.Tcp
                     //this.packetSerializationHandler.Deserialize(messageId, ms);
                 }
             }
+        }
+
+        private void Foo()
+        {
+            var asmName = new AssemblyName("MessageClient_" + typeof(T).Name);
+            var dynAsm = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndSave);
+
+            var typeB = dynAsm.DefineDynamicModule("MessageClient_" + typeof(T).Name + ".dll")
+                              .DefineType(typeof(T).Name + "Impl", TypeAttributes.Public,
+                                          null, new[] { typeof(T)});
+
+            foreach (var property in typeof(T).GetProperties()
+                                                .Where(p => p.PropertyType.GetGenericTypeDefinition() == typeof(IObservable<>)))
+            {
+                var obsType = property.PropertyType.GetGenericArguments().First();
+
+                var propB = typeB.DefineProperty(property.Name, PropertyAttributes.HasDefault,
+                                    property.PropertyType, null);
+
+                var mb = typeB.DefineMethod("get_" + property.Name,
+                                                MethodAttributes.Public |
+                                                    MethodAttributes.SpecialName |
+                                                    MethodAttributes.HideBySig | 
+                                                    MethodAttributes.Virtual, 
+                                                property.PropertyType, 
+                                                Type.EmptyTypes);
+                var ilGen = mb.GetILGenerator();
+
+                ilGen.Emit(OpCodes.Ldnull);
+                ilGen.Emit(OpCodes.Ret);
+
+                propB.SetGetMethod(mb);
+            }
+
+
+            var myType = typeB.CreateType();
+
+            dynAsm.Save(asmName.Name + ".dll");
+            var t = (T)Activator.CreateInstance(myType);
         }
     }
 }
