5ae84de6bb9f1e92f39e186d32d2da45fae1f07a
Marcin Deptuła <marcin.deptula@live.com>
2014-06-08 18:43:47 +0200

    Refactoring (part 2)

--- Stacks/Client/Tcp/ReactiveMessageClient.cs
+++ Stacks/Client/Tcp/ReactiveMessageClient.cs
@@ -6,8 +6,6 @@ using System.Net;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,19 +14,21 @@ namespace Stacks.Tcp
     public class ReactiveMessageClient<T> : MessageClientBase
     {
         private Dictionary<int, Action<MemoryStream>> deserializeByMessageId;
+        private ReactiveMessageReceiverCreator<T> messageReceiverCreator;
 
 
         public T Packets { get; private set; }
+
 
         public ReactiveMessageClient(IFramedClient framedClient,
                                      IStacksSerializer packetSerializer)
             : base(framedClient, new MessageIdCache(), packetSerializer)
         {
-            this.deserializeByMessageId = new Dictionary<int, Action<MemoryStream>>();
+            this.messageReceiverCreator = new ReactiveMessageReceiverCreator<T>(base.messageIdCache, packetSerializer);
+            
+            this.Packets = this.messageReceiverCreator.CreateReceiverImplementation(out deserializeByMessageId);
 
             this.framedClient.Received.Subscribe(PacketReceived);
-
-            PrepareReceiverImplementation();
         }
 
         private unsafe void PacketReceived(ArraySegment<byte> buffer)
@@ -53,101 +53,5 @@ namespace Stacks.Tcp
             }
         }
 
-        private Dictionary<string, object> subjectsToInject;
-        private TypeBuilder tb;
-
-        private void PrepareReceiverImplementation()
-        {
-            tb = CreateTypeBuilder();
-            subjectsToInject = new Dictionary<string, object>();
-
-            foreach (var property in GetTypeObservableProperties())
-            {
-                ParseProperty(property);
-            }
-
-            var myType = tb.CreateType();
-
-            var packetsImplementation = (T)Activator.CreateInstance(myType);
-
-            InjectsSubjectsToImpl(myType, packetsImplementation);
-
-            this.Packets = packetsImplementation;
-        }
-
-        private void SetupDeserializeAction<V>(int messageId, Subject<V> subject)
-        {
-            this.deserializeByMessageId[messageId] = ms =>
-                {
-                    var t = base.packetSerializer.Deserialize<V>(ms);
-                    subject.OnNext(t);
-                };
-        }
-
-        private TypeBuilder CreateTypeBuilder()
-        {
-            var asmName = new AssemblyName("MessageClient_" + typeof(T).Name);
-            var dynAsm = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndCollect);
-
-            var typeB = dynAsm.DefineDynamicModule("MessageClient_" + typeof(T).Name + ".dll")
-                              .DefineType(typeof(T).Name + "Impl", TypeAttributes.Public,
-                                          null, new[] { typeof(T) });
-
-            return typeB;
-        }
-
-        private void ParseProperty(PropertyInfo property)
-        {
-            var packetType = property.PropertyType.GetGenericArguments().First();
-            int messageId = base.messageIdCache.GetMessageId(packetType);
-
-            var subjectType = typeof(Subject<>).MakeGenericType(packetType);
-            var subject = Activator.CreateInstance(subjectType);
-
-            var pName = property.Name;
-            var fieldName = pName + "field";
-
-            subjectsToInject[fieldName] = subject;
-
-            typeof(ReactiveMessageClient<T>)
-                .GetMethod("SetupDeserializeAction", BindingFlags.Instance | BindingFlags.NonPublic)
-                .MakeGenericMethod(packetType)
-                .Invoke(this, new[] { messageId, subject });
-
-
-            var fb = tb.DefineField(fieldName, subjectType, FieldAttributes.Private);
-            var pb = tb.DefineProperty(pName, PropertyAttributes.HasDefault,
-                                          property.PropertyType, null);
-            var mb = tb.DefineMethod("get_" + pName,
-                                        MethodAttributes.Public |
-                                            MethodAttributes.SpecialName |
-                                            MethodAttributes.HideBySig |
-                                            MethodAttributes.Virtual,
-                                        property.PropertyType,
-                                        Type.EmptyTypes);
-            var ilGen = mb.GetILGenerator();
-
-            ilGen.Emit(OpCodes.Ldarg_0);
-            ilGen.Emit(OpCodes.Ldfld, fb);
-
-            ilGen.Emit(OpCodes.Call, typeof(Observable).GetMethod("AsObservable", BindingFlags.Public | BindingFlags.Static).MakeGenericMethod(packetType));
-            ilGen.Emit(OpCodes.Ret);
-
-            pb.SetGetMethod(mb);
-        }
-
-        private void InjectsSubjectsToImpl(Type realType, T impl)
-        {
-            foreach (var kv in subjectsToInject)
-            {
-                realType.GetField(kv.Key, BindingFlags.Instance | BindingFlags.NonPublic).SetValue(impl, kv.Value);
-            }
-        }
-
-        IEnumerable<PropertyInfo> GetTypeObservableProperties()
-        {
-            return typeof(T).GetProperties()
-                            .Where(p => p.PropertyType.GetGenericTypeDefinition() == typeof(IObservable<>));
-        }
     }
 }
---
+++ Stacks/MessageClient/ReactiveMessageReceiverCreator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class ReactiveMessageReceiverCreator<T>
+    {
+        private Dictionary<string, object> subjectsToInject;
+        private TypeBuilder tb;
+
+        private readonly IMessageIdCache messageIdCache;
+        private readonly IStacksSerializer packetSerializer;
+
+        public ReactiveMessageReceiverCreator(IMessageIdCache messageIdCache,
+            IStacksSerializer packetSerializer)
+        {
+            this.messageIdCache = messageIdCache;
+            this.packetSerializer = packetSerializer;
+        }
+
+        public T CreateReceiverImplementation(out Dictionary<int, Action<MemoryStream>> deserializeByMessageId)
+        {
+            tb = CreateTypeBuilder();
+            subjectsToInject = new Dictionary<string, object>();
+
+            deserializeByMessageId = new Dictionary<int, Action<MemoryStream>>();
+
+            foreach (var property in GetTypeObservableProperties())
+            {
+                ParseProperty(property, deserializeByMessageId);
+            }
+
+            var myType = tb.CreateType();
+
+            var packetsImplementation = (T)Activator.CreateInstance(myType);
+
+            InjectsSubjectsToImpl(myType, packetsImplementation);
+
+            return packetsImplementation;
+        }
+
+        private TypeBuilder CreateTypeBuilder()
+        {
+            var asmName = new AssemblyName("MessageClient_" + typeof(T).Name);
+            var dynAsm = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndCollect);
+
+            var typeB = dynAsm.DefineDynamicModule("MessageClient_" + typeof(T).Name + ".dll")
+                              .DefineType(typeof(T).Name + "Impl", TypeAttributes.Public,
+                                          null, new[] { typeof(T) });
+
+            return typeB;
+        }
+
+        private void ParseProperty(PropertyInfo property, Dictionary<int, Action<MemoryStream>> deserializeByMessageId)
+        {
+            var packetType = property.PropertyType.GetGenericArguments().First();
+            int messageId = this.messageIdCache.GetMessageId(packetType);
+
+            var subjectType = typeof(Subject<>).MakeGenericType(packetType);
+            var subject = Activator.CreateInstance(subjectType);
+
+            var pName = property.Name;
+            var fieldName = pName + "field";
+
+            subjectsToInject[fieldName] = subject;
+
+            deserializeByMessageId[messageId] = (Action<MemoryStream>)
+                typeof(ReactiveMessageReceiverCreator<T>)
+                    .GetMethod("SetupDeserializeAction", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .MakeGenericMethod(packetType)
+                    .Invoke(this, new[] { subject });
+
+
+            var fb = tb.DefineField(fieldName, subjectType, FieldAttributes.Private);
+            var pb = tb.DefineProperty(pName, PropertyAttributes.HasDefault,
+                                          property.PropertyType, null);
+            var mb = tb.DefineMethod("get_" + pName,
+                                        MethodAttributes.Public |
+                                            MethodAttributes.SpecialName |
+                                            MethodAttributes.HideBySig |
+                                            MethodAttributes.Virtual,
+                                        property.PropertyType,
+                                        Type.EmptyTypes);
+            var ilGen = mb.GetILGenerator();
+
+            ilGen.Emit(OpCodes.Ldarg_0);
+            ilGen.Emit(OpCodes.Ldfld, fb);
+
+            ilGen.Emit(OpCodes.Call, typeof(Observable).GetMethod("AsObservable",
+                                                                  BindingFlags.Public |
+                                                                    BindingFlags.Static)
+                                                       .MakeGenericMethod(packetType));
+            ilGen.Emit(OpCodes.Ret);
+
+            pb.SetGetMethod(mb);
+        }
+
+        private void InjectsSubjectsToImpl(Type realType, T impl)
+        {
+            foreach (var kv in subjectsToInject)
+            {
+                realType.GetField(kv.Key, BindingFlags.Instance | 
+                                            BindingFlags.NonPublic)
+                        .SetValue(impl, kv.Value);
+            }
+        }
+
+        IEnumerable<PropertyInfo> GetTypeObservableProperties()
+        {
+            return typeof(T).GetProperties()
+                            .Where(p => p.PropertyType
+                                         .GetGenericTypeDefinition() == typeof(IObservable<>));
+        }
+
+        private Action<MemoryStream> SetupDeserializeAction<V>(Subject<V> subject)
+        {
+            return ms =>
+            {
+                var t = this.packetSerializer.Deserialize<V>(ms);
+                subject.OnNext(t);
+            };
+        }
+    }
+}
