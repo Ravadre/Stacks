a99c92dd0577597a3099dd0d59a6490f9708ae64
Marcin Deptuła <marcin.deptula@live.com>
2014-07-12 02:23:37 +0200

    Added F# library and sample projects

---
+++ Examples/ActorFSharpSample2/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+open System.Net.Http
+open Stacks.Actors
+open System.Xml.Linq
+open Stacks
+
+type Weather() = 
+    let actor = ActorContext("Weather")
+    let httpClient = new HttpClient()
+
+    let xn s = XName.Get(s)
+
+    member this.GetTemperature(city: string) = async {
+        do! Async.SwitchToContext(actor.Context)
+
+        let! response = sprintf "http://api.openweathermap.org/data/2.5/\
+                                 weather?q=%s&mode=xml&units=metric" city
+                        |> httpClient.GetStringAsync
+                        |> Async.AwaitTask
+
+        return double (XDocument.Parse(response)
+                                .Element(xn"current")
+                                .Element(xn"temperature")
+                                .Attribute(xn"value"))
+    }
+
+
+[<EntryPoint>]
+let main argv = 
+    
+    let weather = Weather()
+
+    try
+        let temp = weather.GetTemperature("Warsaw") |> Async.RunSynchronously
+        printfn "Temperature in Warsaw, Poland: %.2f\u00B0C" temp
+    with
+    | e -> printfn "Could not get temperature for Warsaw, Poland"
+
+    0
---
+++ Stacks.FSharp/Actors/ActorContext.fs
@@ -0,0 +1,5 @@
+﻿namespace Stacks.FSharp
+
+
+type Class1() = 
+    member this.X = "F#"
