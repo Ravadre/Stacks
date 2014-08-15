43583cc6361e6f784cca62199f5220198cc8241e
Marcin Deptuła <marcin.deptula@live.com>
2014-08-15 13:30:12 +0200

    Actor context provides executor error event

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cls
 .\tools\NuGet\nuget.exe install FAKE -OutputDirectory tools\ -ExcludeVersion
-.\tools\FAKE\tools\Fake.exe Build.fsx "All"
+.\tools\FAKE\tools\Fake.exe Build.fsx "All" %*
