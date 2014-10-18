b95af0640d43b6da9a6ad7d3523a6d962915b86d
Marcin Deptuła <marcin.deptula@live.com>
2014-10-18 17:43:34 +0200

    Updated to version 1.1.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
--- SharedAssemblyInfo.fs
+++ SharedAssemblyInfo.fs
@@ -11,6 +11,6 @@ open System.Runtime.InteropServices
 
 [<assembly: ComVisible(false)>]
 
-[<assembly: AssemblyVersion("1.0.1")>]
-[<assembly: AssemblyFileVersion("1.0.1")>]
+[<assembly: AssemblyVersion("1.1.0")>]
+[<assembly: AssemblyFileVersion("1.1.0")>]
 do()
