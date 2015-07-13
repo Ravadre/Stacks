80fe505f78c31a0d11d34ccc62a32148281e2f24
Marcin Deptuła <marcin.deptula@live.com>
2015-07-13 21:28:03 +0200

    Added parameter test

--- Stacks.Tests/ActorSystemTests/ActorCodeGeneration.cs
+++ Stacks.Tests/ActorSystemTests/ActorCodeGeneration.cs
@@ -19,9 +19,20 @@ namespace Stacks.Tests.ActorSystemTests
         public void Actor_with_single_method_returning_task_should_compile_successfully()
         {
             var actor = ActorSystem.Default.CreateActor<ISingleMethodActor, SingleMethodActor>();
-            var t = actor.Test();
             Assert.Equal(5, actor.Test().Result);
         }
+
+        [Fact]
+        public void Actor_with_method_with_parameters_should_compile_successfully()
+        {
+            var actor = ActorSystem.Default.CreateActor<ICalculatorActor, CalculatorActor>();
+            Assert.Equal(30, actor.Div(150, 5).Result);
+        }
+    }
+
+    public interface ICalculatorActor
+    {
+        Task<double> Div(double x, double y);
     }
 
     public interface ISingleMethodActor
@@ -36,6 +47,16 @@ namespace Stacks.Tests.ActorSystemTests
             await Context;
 
             return 5;
+        }
+    }
+
+    public class CalculatorActor : Actor, ICalculatorActor
+    {
+        public async Task<double> Div(double x, double y)
+        {
+            await Context;
+
+            return x / y;
         }
     }
 }
