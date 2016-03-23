eb9541a1dee71a641756d173802f3c381c901d33
Marcin Deptuła <marcin.deptula@live.com>
2016-03-23 18:07:57 +0100

    Bumped version

--- SharedAssemblyInfo.fs
+++ SharedAssemblyInfo.fs
@@ -11,6 +11,6 @@ open System.Runtime.InteropServices
 
 [<assembly: ComVisible(false)>]
 
-[<assembly: AssemblyVersion("2.0.0")>]
-[<assembly: AssemblyFileVersion("2.0.0")>]
+[<assembly: AssemblyVersion("2.0.1")>]
+[<assembly: AssemblyFileVersion("2.0.1")>]
 do()
